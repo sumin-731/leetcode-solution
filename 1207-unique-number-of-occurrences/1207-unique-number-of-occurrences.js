df6706d663fcf1ee9b0c024a2638958cc1ef9551
sumin-731 <64387907+sumin-731@users.noreply.github.com>
2025-02-01 17:41:17 +0900

    Time: 10 ms (5.31%), Space: 54.3 MB (5.63%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
@@ -3,7 +3,8 @@
  * @return {boolean}
  */
 var uniqueOccurrences = function(arr) {
-    let originLength = arr.length;
+    //문제 오해..!
+    /*let originLength = arr.length;
     const newSet = new Set(arr);
     let setLength = newSet.size;
 
@@ -11,5 +12,28 @@ var uniqueOccurrences = function(arr) {
         return false;
     }else{
         return true;
+    }*/
+
+    //주어진 배열 arr에서 '각 숫자의 등장 횟수'가 모두 유일한지 확인하는 문제
+    let tNumArr = [];
+    while(true){
+        let restArr = arr.filter((elem) => elem != arr[0]);
+
+        tNumArr.push(arr.length - restArr.length);
+        arr = restArr;
+
+        if(restArr.length == 0){
+            break;
+        }
+    }
+
+    let originLength = tNumArr.length;
+    const newSet = new Set(tNumArr);
+    let setLength = newSet.size;
+
+    if(originLength == setLength){
+        return true;
+    }else{
+        return false;
     }
 };
